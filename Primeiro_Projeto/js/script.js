cad171e5f38266275519f6e935e85ba201b7c5e2
KarinaRV <karinaricioni39@gmail.com>
2021-11-26 01:38:47 -0300

    DevQuiz com Javascript

---
+++ Primeiro_Projeto/js/script.js
@@ -0,0 +1,21 @@
+//Home - index
+function btnJava() {
+    document.getElementById("btnClickJava").submit();
+}
+
+function btnMySQL() {
+    document.getElementById("btnClickMySQL").submit();
+}
+
+//Quiz MySQL
+function btnFinalMySQL(){
+    document.getElementById("btnIdFinalMySQL").submit();
+}
+function btnHome(){
+    document.getElementById("btnHome").submit();
+}
+
+//Quiz Java
+function btnFinalJava() {
+    document.getElementById("btnIdFinalJava").submit();
+}
